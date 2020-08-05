5bfde971084fc5bed74301b378cd2a51b690d649
Luke Towell <luke.towell@hotmail.com>
2020-08-05 16:08:24 +0100

    Added primary greeting service
    - added a primary greeting service implementation

---
+++ src/main/java/com/luketowell/depinjection/services/PrimaryGreetingServiceImpl.java
@@ -0,0 +1,15 @@
+package com.luketowell.depinjection.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingServiceImpl implements GreetingService{
+
+    @Override
+    public String sayGreeting(){
+        return "Hello World - Primary";
+    }
+
+}
