755e6884a5c3e119858fa896a8bf43449ec747b9
Luke Towell <luke.towell@hotmail.com>
2020-08-05 15:53:15 +0100

    Added in a greeting service and property based implementation
    - Added in a greeting service with a sayGreeting method
    - Added in a property based implementation for the sayGreeting method.
    - Added controller which calls the service and qualifies with the property based name in order to ensure the correct service implementation is used.

---
+++ src/main/java/com/luketowell/depinjection/controllers/PropertyInjectedController.java
@@ -0,0 +1,18 @@
+package com.luketowell.depinjection.controllers;
+
+import com.luketowell.depinjection.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+    @Qualifier("propertyGreetingServiceImpl")
+    @Autowired
+    public GreetingService greetingService;
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/com/luketowell/depinjection/services/GreetingService.java
@@ -0,0 +1,8 @@
+package com.luketowell.depinjection.services;
+
+import org.springframework.stereotype.Service;
+
+
+public interface GreetingService {
+    String sayGreeting();
+}
