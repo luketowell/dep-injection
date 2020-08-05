58ec62316424eb3b56d089f1af7f4dbefc137434
Luke Towell <luke.towell@hotmail.com>
2020-08-05 15:54:09 +0100

    Added property controller tests
    - added property controller tests for the property controller

---
+++ src/test/java/com/luketowell/depinjection/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package com.luketowell.depinjection.controllers;
+
+import com.luketowell.depinjection.services.ConstructorGreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController controller;
+
+    @BeforeEach
+    void setUp(){
+        controller= new PropertyInjectedController();
+        controller.greetingService = new ConstructorGreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
