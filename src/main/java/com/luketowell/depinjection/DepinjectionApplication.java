772c72d7685c4c4ee544d6871e21bc4d4bd43a3e
Luke Towell <luke.towell@hotmail.com>
2020-08-05 12:32:22 +0100

    Initial Commit

---
+++ src/main/java/com/luketowell/depinjection/DepinjectionApplication.java
@@ -0,0 +1,13 @@
+package com.luketowell.depinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepinjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepinjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luketowell/depinjection/DepinjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luketowell.depinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepinjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
